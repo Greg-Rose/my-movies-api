dce4a8922c768a0dcf66ab78516b8306cab85379
Greg Rose <gregrose628@gmail.com>
2018-01-04 19:52:53 -0500

    added API Version, removed unused files

---
+++ app/controllers/v1/movies_controller.rb
@@ -0,0 +1,18 @@
+module V1
+  class MoviesController < ApplicationController
+    def discover
+      movies = TMDB::Movie.discover
+      json_response(movies)
+    end
+
+    def search
+      movies = TMDB::Movie.search(params[:query])
+      json_response(movies)
+    end
+
+    def find
+      movie = TMDB::Movie.find(params[:tmdb_id])
+      json_response(movie)
+    end
+  end
+end
---
+++ app/controllers/v1/my_movies_controller.rb
@@ -0,0 +1,62 @@
+module V1
+  class MyMoviesController < ApplicationController
+    # GET /my_watched_movies
+    def watched
+      @watched_movies = current_user.movies.watched
+      json_response(@watched_movies)
+    end
+
+    # GET /my_movies_to_watch
+    def to_watch
+      @movies_to_watch = current_user.movies.to_watch
+      json_response(@movies_to_watch)
+    end
+
+    # POST /my_movies
+    def create
+      # find or create movie in database
+      movie = Movie.find_or_initialize_by(movie_params)
+
+      if movie.new_record?
+        movie.release_date = DateTime.parse(params[:release_date]) if params[:release_date]
+        movie.save!
+        params[:genres].each { |g| movie.genres.find_or_create_by!(name: g["name"]) }
+      end
+
+      # create my_movie belonging to current user
+      watched = params["watched"]
+      to_watch = !params["watched"]
+      @my_movie = current_user.my_movies.create!(movie: movie, watched: watched, to_watch: to_watch)
+      json_response(@my_movie, :created)
+    end
+
+    # PUT /my_movies/:id
+    def update
+      @my_movie = MyMovie.find(params[:id])
+      raise ExceptionHandler::AuthenticationError if @my_movie.user != current_user
+
+      if params["watched"]
+        @my_movie.update_attributes(watched: true, to_watch: false)
+      else
+        @my_movie.update_attributes(update_params)
+      end
+
+      if !@my_movie.watched && !@my_movie.to_watch
+        @my_movie.destroy
+        head :no_content
+      else
+        json_response(@my_movie, :accepted)
+      end
+    end
+
+    private
+
+    def movie_params
+      params.permit(:title, :tmdb_id, :runtime, :poster_path)
+    end
+
+    def update_params
+      params.permit(:watched, :to_watch)
+    end
+  end
+end
